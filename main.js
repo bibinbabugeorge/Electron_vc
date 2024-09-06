38273d59a137fad96a979e7383eb3a077c41693d
Kirandas__M <kirandasmundakkal@gmail.com>
2024-09-06 11:42:11 +0530

    changes package.json file

--- main.js
+++ main.js
@@ -1,31 +1,21 @@
-const { app, BrowserWindow, ipcMain, session, Menu } = require('electron');
+const { app, BrowserWindow, ipcMain, session } = require('electron');
 const path = require('node:path');
-
-
-Menu.setApplicationMenu(null);
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    icon: './assets/group_icon.ico',
+    width: 800,
     height: 800,
-    width : 800,
     show: false,
     //frame: false,    //hiding frame 
-    //autoHideMenuBar: true,  // hide menu bar
+    autoHideMenuBar: true,  // hide menu bar
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'), // Preload script
+      preload: path.join(__dirname, 'preload'), // Preload script
       nodeIntegration: true,
       contextIsolation: true,
       enableRemoteModule: false,
       //devTools: false    //disble dev tools 
-      devTools: true    //disble dev tools 
     }
   });
-
-  Menu.setApplicationMenu(null);
-
-  mainWindow.loadFile('index.html');
-  mainWindow.webContents.openDevTools();
   mainWindow.loadFile('splash.html');
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
